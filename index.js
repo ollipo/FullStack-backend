08bac101d00ec80f0dcab6939b70be35e12cce17
ojporv <olli.porvari@helsinki.fi>
2021-09-06 10:28:35 +0300

    3.2 tehty

--- index.js
+++ index.js
@@ -24,8 +24,11 @@ let persons = [
   }
 ]
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.get('/info', (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`
+    )
 })
 
 app.get('/api/persons', (req, res) => {
