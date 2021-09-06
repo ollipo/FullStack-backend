b773da6f0a3469bb17bd6a826d8ff8bd6577b05e
ojporv <olli.porvari@helsinki.fi>
2021-09-06 12:49:40 +0300

    3.8 tehty

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+
+morgan.token('data', function (req, res) { return (JSON.stringify(req.body)) })
 
 let persons = [
   {
